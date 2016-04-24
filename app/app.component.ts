c60f8b20fa525095c1c22b8933d755b81b25bf26
Fabian <fcabasan@gmail.com>
2016-04-24 16:35:09 +0200

    routes configuration (dashboard and list)

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,35 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 import {HeroesComponent} from './heroes.component';
+import {DashboardComponent} from './dashboard.component';
 import {HeroService} from './hero.service';
+
+@RouteConfig([
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  }
+])
 
 @Component({
   selector: 'my-app',
   template: `
   <h1>{{title}}</h1>
-  <my-heroes></my-heroes>
-`,
-  directives: [HeroesComponent],
-  providers: [HeroService]
+  <nav>
+    <a [routerLink]="['Dashboard']">Dashboard</a>
+    <a [routerLink]="['Heroes']">Heroes</a>
+  </nav>  <router-outlet></router-outlet>
+  `,
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS, HeroService]
 })
-export class AppComponent{
+export class AppComponent {
   title = 'Tour of Heroes';
 }
